5b3a04f0acb138088452fdd800093dcf780e47b7
flitrue <812863096@qq.com>
2019-09-13 14:52:32 +0800

    :+1: 测试跨月标记

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <div style="260px">
-      <i-calender :marks="marks" />
+    <div style="width: 260px">
+      <i-calender :marks="marks" @forward="change" @back="change" />
     </div>
   </div>
 </template>
@@ -10,30 +10,39 @@
 import iCalender from "icalender";
 
 export default {
-  name: 'app',
+  name: "app",
   data() {
     return {
       marks: [
         {
           start: "2019-09-10",
-          end: "2019-09-10",
+          end: "2019-09-10"
         },
         {
           start: "2019-09-13",
-          end: "2019-09-15",
+          end: "2019-09-15"
+        },
+        {
+          start: "2019-09-28",
+          end: "2019-10-03"
         }
       ]
-    }
+    };
   },
   components: {
     iCalender
+  },
+  methods: {
+    change(date) {
+      console.log(date); // eslint-disable-line
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
